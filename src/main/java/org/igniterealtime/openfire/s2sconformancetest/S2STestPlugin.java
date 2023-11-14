2e5d769970b04fa5992bad8e4350a9b881947796
Guus der Kinderen <guus.der.kinderen@gmail.com>
2023-11-14 12:07:45 +0100

    fixes #6: Use List-based properties

--- src/main/java/org/igniterealtime/openfire/s2sconformancetest/S2STestPlugin.java
+++ src/main/java/org/igniterealtime/openfire/s2sconformancetest/S2STestPlugin.java
@@ -23,6 +23,7 @@ import org.slf4j.LoggerFactory;
 
 import java.io.File;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -37,57 +38,19 @@ public class S2STestPlugin implements Plugin {
     private static final String pluginName = "S2S Conformance Test";
     private static final String pluginPropertyPrefix = "plugin.s2sconformancetest.";
 
-    public static final SystemProperty<String> SUCCESSFUL_DOMAINS = SystemProperty.Builder.ofType(String.class)
+    public static final SystemProperty<List<String>> SUCCESSFUL_DOMAINS = SystemProperty.Builder.ofType(List.class)
             .setPlugin(pluginName)
             .setKey(pluginPropertyPrefix + "successful-domains")
-            .setDefaultValue("xmpp.org")
+            .setDefaultValue(List.of("xmpp.org"))
             .setDynamic(true)
-            .build();
+            .buildList(String.class);
 
-    public static final SystemProperty<String> UNSUCCESSFUL_DOMAINS = SystemProperty.Builder.ofType(String.class)
+    public static final SystemProperty<List<String>> UNSUCCESSFUL_DOMAINS = SystemProperty.Builder.ofType(List.class)
             .setPlugin(pluginName)
             .setKey(pluginPropertyPrefix + "unsuccessful-domains")
-            .setDefaultValue("expired.badxmpp.eu")
+            .setDefaultValue(List.of("expired.badxmpp.eu"))
             .setDynamic(true)
-            .build();
-
-    public void setSuccessfulDomains(String successfulDomains) {
-        SUCCESSFUL_DOMAINS.setValue(successfulDomains);
-    }
-
-    public void setSuccessfulDomains(String[] successfulDomains) {
-        SUCCESSFUL_DOMAINS.setValue(String.join(",", successfulDomains));
-    }
-
-    public String getSuccessfulDomains() {
-        return SUCCESSFUL_DOMAINS.getValue();
-    }
-
-    public String[] getSuccessfulDomainsAsArray() {
-        return Arrays.stream(SUCCESSFUL_DOMAINS.getValue().split(","))
-                .map(String::trim)
-                .filter(x -> !x.isEmpty())
-                .toArray(String[]::new);
-    }
-
-    public void setUnsuccessfulDomains(String unsuccessfulDomains) {
-        UNSUCCESSFUL_DOMAINS.setValue(unsuccessfulDomains);
-    }
-
-    public void setUnsuccessfulDomains(String[] unsuccessfulDomains) {
-        UNSUCCESSFUL_DOMAINS.setValue(String.join(",", unsuccessfulDomains));
-    }
-
-    public String getUnsuccessfulDomains() {
-        return UNSUCCESSFUL_DOMAINS.getValue();
-    }
-
-    public String[] getUnsuccessfulDomainsAsArray() {
-        return Arrays.stream(UNSUCCESSFUL_DOMAINS.getValue().split(","))
-                .map(String::trim)
-                .filter(x -> !x.isEmpty())
-                .toArray(String[]::new);
-    }
+          .buildList(String.class);
 
     public void initializePlugin(PluginManager pluginManager, File file) {
 
@@ -99,12 +62,12 @@ public class S2STestPlugin implements Plugin {
 
     public S2STestResultRun runTests() throws Exception {
         S2STestResultRun thisRun = new S2STestResultRun();
-        for (String domain : getSuccessfulDomainsAsArray()) {
+        for (String domain : SUCCESSFUL_DOMAINS.getValue()) {
             final Map<String,String> thisResult = new S2STestService(domain).run();
             Log.info("S2S Test Result - Domain: [{}] Result: [{}]", domain, thisResult.get("status"));
             thisRun.addSuccessfulResult(new S2STestResult(domain, thisResult));
         }
-        for (String domain : getUnsuccessfulDomainsAsArray()) {
+        for (String domain : UNSUCCESSFUL_DOMAINS.getValue()) {
             final Map<String,String> thisResult = new S2STestService(domain).run();
             Log.info("S2S Test Result - Domain: [{}] Result: [{}]", domain, thisResult.get("status"));
             thisRun.addUnsuccessfulResult(new S2STestResult(domain, thisResult));
